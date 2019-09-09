822aa5309a119fab5f487b8b922e10c83aa44c19
Wilson Carvalho <eng.wilson.c@gmail.com>
2019-09-09 10:06:24 -0300

    Projeto init

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '73097ecab2610a7cb8cc411389ffd84d',
+  expiresIn: '7d',
+};
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'ad551f7dd716ab',
+    pass: '93fe009e3fcfd1',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
