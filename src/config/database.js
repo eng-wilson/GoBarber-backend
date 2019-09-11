365f1371bdd20f6a467e1c5d804955f2f68db5a4
wilson <wilson@Mac-mini-de-wilson.local>
2019-09-11 09:56:23 -0300

    postgres working

--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,9 @@ module.exports = {
   username: 'bogdqklkazyfbj',
   password: '6a0d3e83d8cc89e6768cdd3f70173ff4b77f6e23ddcac07acc81ae3ab5a3908f',
   database: 'duupg5ltd56a8',
+  dialectOptions: {
+    ssl: true,
+  },
   define: {
     timestamps: true,
     underscored: true,
