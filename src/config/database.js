8f9def41d7216e93c5c4160c2d93e268d57be706
wilson <wilson@Mac-mini-de-wilson.local>
2019-09-11 09:11:12 -0300

    postgres config

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'gobarber-252414:us-central1:gobarber-wilson',
-  username: 'postgres',
-  password: 'gobarber',
-  database: 'gobarber-wilson',
+  host: 'ec2-23-23-182-18.compute-1.amazonaws.com',
+  username: 'bogdqklkazyfbj',
+  password: '6a0d3e83d8cc89e6768cdd3f70173ff4b77f6e23ddcac07acc81ae3ab5a3908f',
+  database: 'duupg5ltd56a8',
   define: {
     timestamps: true,
     underscored: true,
